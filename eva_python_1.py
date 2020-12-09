77d1f3ea3d5bc38a6f78536cb8becca973ba8ea9
KylianChasselin <74654924+KylianChasselin@users.noreply.github.com>
2020-12-09 11:40:32 +0100

    essai 2

--- eva_python_1.py
+++ eva_python_1.py
@@ -12,6 +12,8 @@ input()
 
 
 
+#ESSAI 1 :
+
 
 
 # fonction : 
@@ -50,3 +52,41 @@ mot_joueur=[' ',' ',' ',' ',' ',' ']
 motAtrouver(mot_1,mot_2,mot_3,mot_4,mot_joueur)
 
 print(motAtrouver)
+
+
+
+
+#ESSAI 2 :
+
+
+
+
+# fonction : 
+
+import random
+
+def motAtrouver (mot_1,mot_joueur):
+    for i in range (1,7):
+        mot_joueur[i-1] = input("trouve une lettre :  ")
+    return mot_joueur
+
+def mot_vrai (mot_1,mot_joueur):
+    for i in range (1,7):
+        if mot_joueur[i-1] == mot_1 :
+            print("you win")
+        if mot_joueur[i-1] != mot_1 :
+            print("you loose")
+    return 
+    
+# tableaux : 
+
+mot_1 =['c','a','s','t','o','r']
+mot_2 =['c','i','n','e','m','a']
+mot_3 =['c','y','p','r','e','s']
+mot_4 =['c','i','t','r','o','n']
+
+mot_joueur=[' ',' ',' ',' ',' ',' ']
+
+motAtrouver(mot_1,mot_joueur)
+mot_vrai(mot_1,mot_joueur)
+print(mot_joueur)
