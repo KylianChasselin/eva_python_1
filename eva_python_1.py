86f8f7fe15160bb8487d956fd41adaf012789380
KylianChasselin <74654924+KylianChasselin@users.noreply.github.com>
2020-12-09 12:31:26 +0100

    couleur rouge-bleu apparait

--- eva_python_1.py
+++ eva_python_1.py
@@ -1,34 +1,31 @@
 pip install colorama
 
-from colorama import init
-init()
-from colorama import Fore, Back, Style
-
-
 
 #ESSAI 2 :
 
 
-
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 # fonction : 
 
 import random
 
-def motAtrouver (mot_1,mot_joueur):
+def motAtrouver(mot_1,mot_joueur):
     for i in range (1,7):
         mot_joueur[i-1] = input("trouve une lettre :  ")
     return mot_joueur
 
-def mot_vrai (mot_1,mot_joueur):
+def mot_vrai(mot_1,mot_joueur):
     for i in range (1,7):
         if mot_joueur[i-1] == mot_1 :
             print("you win")
         if mot_joueur[i-1] != mot_1 :
             print("you loose")
     return 
-    
-def lettre_bonne (mot_1, mot_joueur):
+
+def lettre_bonne(mot_1, mot_joueur):
     for i in range (1,7):
         if mot_joueur[0] == mot_1[0]:
             print(Back.RED + mot_joueur[0], end=" ")
@@ -44,20 +41,20 @@ def lettre_bonne (mot_1, mot_joueur):
             print(Back.RED + mot_joueur[5], end=" ")
     return lettre_bonne
 
-def lettre_pas_presente (mot_1, mot_joueur):
+def lettre_pas_presente(mot_1, mot_joueur):
     for i in range (1,7):
         if mot_joueur[0] != mot_1[0]:
-            print(Back.blue + mot_joueur[0], end=" ")
+            print(Back.BLUE + mot_joueur[0], end=" ")
         if mot_joueur[1] != mot_1[1]:
-            print(Back.blue + mot_joueur[1], end=" ")
+            print(Back.BLUE + mot_joueur[1], end=" ")
         if mot_joueur[2] != mot_1[2]:
-            print(Back.blue + mot_joueur[2], end=" ")
+            print(Back.BLUE + mot_joueur[2], end=" ")
         if mot_joueur[3] != mot_1[3]:
-            print(Back.blue + mot_joueur[3], end=" ")
+            print(Back.BLUE + mot_joueur[3], end=" ")
         if mot_joueur[4] != mot_1[4]:
-            print(Back.blue + mot_joueur[4], end=" ")
+            print(Back.BLUE + mot_joueur[4], end=" ")
         if mot_joueur[5] != mot_1[5]:
-            print(Back.blue + mot_joueur[5], end=" ")
+            print(Back.BLUE + mot_joueur[5], end=" ")
     return lettre_pas_presente
 
 # tableaux : 
@@ -74,6 +71,7 @@ mot_vrai(mot_1,mot_joueur)
 lettre_bonne(mot_1,mot_joueur)
 lettre_pas_presente(mot_1,mot_joueur)
 
-print(mot_joueur)
+print(motAtrouver)
+
 
 input()
